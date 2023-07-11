2f435be702b28b682b49a3e1a0213f8548525f3e
Jie Chu <105377443+BXZZcj@users.noreply.github.com>
2023-07-11 17:08:20 +0800

    我为严文昊新添加了一个测试用API。具体内容是我修改了Models_temp的内容以及新加了一个WenhaoYan_temp.cs (#2)

--- back_end/Models_temp/Interface.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace back_end
-{
-    public class Instructor
-    {
-        public string? name { get; set; }
-        public string? description { get; set; }
-        public string? img_link { get; set; }
-    }
-}
