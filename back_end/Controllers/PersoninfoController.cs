827a25562b62e0e2efd6d998fc4723a9232237f3
Shuyu Wang <1491456253@qq.com>
2023-07-11 21:14:56 +0800

    增加了修改个人信息的API

--- back_end/Controllers/PersoninfoController.cs
+++ back_end/Controllers/PersoninfoController.cs
@@ -15,13 +15,46 @@ namespace back_end.Controllers
             _context = context;
         }
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<object>>> GetRegistrations(string PatientID)//查询每个给定日期各个时间段都有多少人挂号，学号是多少
+        public async Task<ActionResult<IEnumerable<object>>> GetRegistrations(string PatientID)//给定学号查询个人信息
         {
             var result = await _context.Patients.Where(r => r.PatientId == PatientID).ToListAsync();
 
             var json = JsonSerializer.Serialize(result);
 
             return Content(json, "application/json");
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> UpdatePatient(string id, Patient patient)//给定学号和要修改的信息修改个人信息
+        {
+            if (id != patient.PatientId){//首先检查传入的病人ID是否与Patient对象中的ID匹配
+                return BadRequest();
+            }
+
+            _context.Entry(patient).State = EntityState.Modified;//标记已修改
+
+            try{
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)//如果在保存更改时发生了并发冲突（例如，另一个用户同时修改了同一条记录）
+                                                //，那么EF Core会抛出一个DbUpdateConcurrencyException异常。
+            {
+                if (!PatientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool PatientExists(string id)
+        {
+            return _context.Patients.Any(e => e.PatientId == id);
         }
     }
 }
