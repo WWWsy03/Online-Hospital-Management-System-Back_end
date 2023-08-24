2f299ae325d250d6049e34deede194b4fd90d7ff
Jie Chu <3325772489@qq.com>
2023-08-24 22:00:31 +0800

    I adjust the PatientController.cs

--- back_end/Controllers/PatientController.cs
+++ back_end/Controllers/PatientController.cs
@@ -14,19 +14,51 @@ namespace back_end.Controllers
             _context = context;
         }
 
+        //插入医生信息
+        [HttpPost("add1")]
+        public async Task<ActionResult<TreatmentRecord>> PostTreatmentRecord(TreatmentRecord row)
+        {
+            _context.TreatmentRecords.Add(row);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("PostTreatmentRecord", new { id = row.DoctorId }, row);
+        }
+
+        [HttpGet("get1")]
+        public async Task<ActionResult<IEnumerable<TreatmentRecord>>> GetTreatmentRecords()
+        {
+            return await _context.TreatmentRecords.ToListAsync();
+        }
+
         [HttpGet("AllPatients")]
         public async Task<ActionResult<IEnumerable<object>>> GetAllPatients()
         {
-            var administrators = await _context.Patients
-                .Select(a => new { a.PatientId, a.Name, a.Gender, a.BirthDate,a.Contact,a.Password,a.College,a.Counsellor })
+            var result = await _context.Patients
+                .Join(
+                    _context.TreatmentRecords,
+                    patient => patient.PatientId,
+                    record1 => record1.PatientId,
+                    (patient, record1) => new { patient, record1 }
+                )
+                .Join(
+                    _context.TreatmentRecord2s,
+                    combined => combined.record1.DiagnosisRecordId,
+                    record2 => record2.DiagnoseId,
+                    (combined, record2) => new
+                    {
+                        PatientId = combined.patient.PatientId,
+                        Name = combined.patient.Name,
+                        DiagnosisTime = record2.DiagnoseTime
+                    }
+                )
                 .ToListAsync();
 
-            if (!administrators.Any())
+            if (!result.Any())
             {
                 return NotFound();
             }
 
-            return Ok(administrators);
+            return Ok(result);
         }
 
 
