f3c9c64ee528603f939e3f18ea735b8b54bc8fc9
1491456253 <1491456253@qq.com>
2023-08-25 11:45:56 +0800

    增加确认就诊API和评价API

---
+++ back_end/Controllers/CommentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using back_end.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommentController : ControllerBase
+    {
+        private readonly ModelContext _context;
+
+        public CommentController(ModelContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateFeedback(string diagnoseId, decimal treatmentScore, string evaluation)
+        {
+            // 从诊断记录ID中分离出病人ID和医生ID
+            var patientId = diagnoseId.Substring(8, 7);
+            var doctorId = diagnoseId.Substring(15, 5);
+
+            // 创建就诊反馈记录
+            var treatmentFeedback = new TreatmentFeedback
+            {
+                PatientId = patientId,
+                DoctorId = doctorId,
+                TreatmentScore = treatmentScore,
+                Evaluation = evaluation
+            };
+
+            try
+            {
+                _context.TreatmentFeedbacks.Add(treatmentFeedback);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok("Treatment feedback created successfully.");
+        }
+    }
+}
---
+++ back_end/Controllers/ConfirmController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using back_end.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TreatmentController : ControllerBase
+    {
+        private readonly ModelContext _context;
+        private static readonly object _lock = new object();
+
+        public TreatmentController(ModelContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateTreatment(string doctorId, string patientId, int leaveDays)
+        {
+            lock (_lock)//加锁，防止有多个用户同时向其中添加记录
+            {
+                // 生成诊断记录ID
+                var diagnoseId = DateTime.Now.ToString("yyyyMMdd") + patientId + doctorId;
+
+
+                // 创建就诊记录
+                var treatmentRecord = new TreatmentRecord
+                {
+                    DiagnosisRecordId = diagnoseId,
+                    DoctorId = doctorId,
+                    PatientId = patientId,
+                    LeaveNoteId = leaveDays > 0 ? diagnoseId + "9" + leaveDays.ToString("D3") : null
+                };
+
+                // 创建就诊记录时间
+                var treatmentRecord2 = new TreatmentRecord2
+                {
+                    DiagnoseId = diagnoseId,
+                    DiagnoseTime = DateTime.Now
+                };
+
+                try
+                {
+                    _context.TreatmentRecords.Add(treatmentRecord);
+                    _context.TreatmentRecord2s.Add(treatmentRecord2);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("An error occurred: " + ex.Message);
+
+                    // 如果有内部异常，打印内部异常的信息
+                    // if (ex.InnerException != null)
+                   // {
+                        // Console.WriteLine("Inner exception: " + ex.InnerException.Message);
+                        //}
+                        return BadRequest(ex.Message);
+                     
+                }
+
+
+                return Ok("Treatment record created successfully.");
+            }
+        }
+    }
+}
