0ba727adb640a3edddcb2cc2c3686846925e331b
1491456253 <1491456253@qq.com>
2023-08-31 15:54:24 +0800

    增添修改药品库存、新增药品库存、获取被清理的药品API

--- back_end/Controllers/MedicineController.cs
+++ back_end/Controllers/MedicineController.cs
@@ -39,5 +39,72 @@ namespace back_end.Controllers
                 .ToListAsync();
         }
 
+        [HttpPut("UpdateStock")]//更新库存
+        public async Task<IActionResult> UpdateStock(string medicineName, decimal newAmount)
+        {
+            var medicineStock = await _context.MedicineStocks.FirstOrDefaultAsync(m => m.MedicineName == medicineName);
+            if (medicineStock == null)
+            {
+                return NotFound("未找到该药品");
+            }
+
+            medicineStock.MedicineAmount = newAmount;
+
+            await _context.SaveChangesAsync();
+
+            return Ok("Medicine stock updated successfully.");
+        }
+
+        [HttpPost("AddStock")]
+        public async Task<IActionResult> AddStock(AddStockInputModel inputModel)
+        {
+            var medicineStock = new MedicineStock
+            {
+                MedicineName = inputModel.MedicineName,
+                Manufacturer = inputModel.Manufacturer,
+                ProductionDate = inputModel.ProductionDate,
+                MedicineShelflife = inputModel.MedicineShelflife,
+                MedicineAmount = inputModel.MedicineAmount,
+                ThresholdValue = inputModel.ThresholdValue
+            };
+
+            _context.MedicineStocks.Add(medicineStock);
+
+            await _context.SaveChangesAsync();
+
+            return Ok("Medicine stock added successfully.");
+        }
+        [HttpGet("GetCleanedMedicines")]
+        public IActionResult GetCleanedMedicines()
+        {
+            var cleanedMedicines = _context.MedicineStocks.Where(m => m.CleanDate != null).ToList();
+
+            var result = cleanedMedicines.Select(m =>
+            {
+                var administrator = _context.Administrators.FirstOrDefault(a => a.AdministratorId == m.CleanAdministrator);
+
+                return new
+                {
+                    MedicineName = m.MedicineName,
+                    CleanDate = m.CleanDate,
+                    administratorId=m.CleanAdministrator,
+                    CleanAdministrator = administrator?.Name
+                };
+            });
+
+            return Ok(result);
+        }
     }
+
+    public class AddStockInputModel
+    {
+        public string MedicineName { get; set; }
+        public string Manufacturer { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public decimal MedicineShelflife { get; set; }
+        public decimal? MedicineAmount { get; set; }
+        public decimal ThresholdValue { get; set; }
+    }
+
 }
+
