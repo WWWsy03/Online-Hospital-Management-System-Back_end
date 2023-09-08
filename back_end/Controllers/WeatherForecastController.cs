a861ed07c0460809d738c8d78a2c390e33b54e4d
Jie Chu <3325772489@qq.com>
2023-09-08 20:42:21 +0800

    I do some change about the API route

--- back_end/Controllers/WeatherForecastController.cs
+++ back_end/Controllers/WeatherForecastController.cs
@@ -2,8 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace back_end.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
